07570081830f82afa3c39ea0a10ca65f37b3d8fd
Zhang Yuzheng <me@zhanyuzheng.com>
2024-01-28 22:45:07 +0800

    [Screener:feature] init front end framework
    Create a UI using next.js
    
    Signed-off-by: Zhang Yuzheng <me@zhanyuzheng.com>

--- minerscreener/src/app/page.js
+++ minerscreener/src/app/page.js
@@ -1,15 +1,9 @@
-// import Image from "next/image";
+import Chart from "@/app/ui/Chart"
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
-      <div
-        id="spx_width"
-        className="z-10 w-full items-center justify-between font-mono text-sm lg:flex"
-      >
-
-        Hello, Miner !!!
-      </div>
-    </main>
-  );
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <Chart/>
+        </main>
+    );
 }
