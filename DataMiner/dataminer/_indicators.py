1ac0429580b1eb2552c8b10ebb0e9ad316bd362b
Yuzheng Zhang <me@zhanyuzheng.com>
2025-06-07 23:24:10 -0400

    DataMiner:feat: add ema support
    1. calculate ema
    2. refine Indicator code
    3. improve test/run.sh, add packges to PYTHONPATH
    
    Signed-off-by: Yuzheng Zhang <me@zhanyuzheng.com>

--- DataMiner/dataminer/_indicators.py
+++ DataMiner/dataminer/_indicators.py
@@ -1,8 +1,14 @@
-from datetime import datetime
+"""
+TODO:
+    - make it a singleton class
+    - improve db query performance
+"""
 
 import os
-from multiprocessing import Pool
 from concurrent.futures import ThreadPoolExecutor
+from datetime import datetime
+from multiprocessing import Pool
+from typing import Literal, Dict
 
 import numpy
 from detonator import is_in_daemon, make_db_connection, get_logger, mongo_2_df, SingletonParent
@@ -13,35 +19,41 @@ from .models import TickerDailyInfo, IndexTickers
 _logger = get_logger('Indicators')
 
 
-def _get_since_trade_date_for_sma(ticker: str, interval: str = '1d', period: int = 20) -> datetime | None:
+def _get_since_trade_date_for_indicator(ticker: str, indicator: Literal['sma', 'ema'] = 'sma', interval: str = '1d',
+                                        period: int = 20) -> datetime | None:
+    """
+    Generic function to get the since trade date for any indicator.
+    """
     ticker = ticker.upper()
-    query = {'ticker__iexact': ticker,
-             'interval__iexact': interval,
-             f'sma{period}__exists': True}
+    query = {
+        'ticker__iexact': ticker,
+        'interval__iexact': interval,
+        f'{indicator}{period}__exists': True
+    }
 
-    infos: QuerySet = TickerDailyInfo.objects(**query)
+    infos: QuerySet = TickerDailyInfo.objects(**query).order_by('-trade_date').limit(1)
     if infos.count() == 0:
-        # 对应的均线从来没有计算过，从头开始计算，返回 None
         return None
-    info: TickerDailyInfo = infos.order_by(
-        '-trade_date').first()  # latest info with sma
+    info: TickerDailyInfo = infos.first()
 
-    query = {'ticker__iexact': ticker,
-             'interval__iexact': interval,
-             'trade_date__lte': info.trade_date}
-    info = TickerDailyInfo.objects(
-        **query).order_by('-trade_date').skip(period).first()
+    query = {
+        'ticker__iexact': ticker,
+        'interval__iexact': interval,
+        'trade_date__lte': info.trade_date
+    }
+    info = TickerDailyInfo.objects(**query).order_by('-trade_date').skip(period).first()
     if info is None:
         _logger.warning(
-            'Illegal state _get_since_trade_date_for_sma for %s', ticker)
+            'Illegal state _get_since_trade_date_for_indicator for %s (%s)', ticker, indicator)
         return None
-    _logger.debug('since %s of %s for sma%d', info.trade_date, ticker, period)
+    _logger.debug('since %s of %s for %s%d', info.trade_date, ticker, indicator, period)
     return info.trade_date
 
 
-def _calculate_sma(ticker: str, since: str | datetime = None, interval='1d', period: int = 20):
+def _calculate_indicator(ticker: str, indicator: Literal['sma', 'ema'] = 'sma', since: str | datetime = None,
+                         interval: str = '1d', period: int = 20):
     _logger.info(
-        'Calculating sma for %s since %s @ interval:%s period:%d', ticker, since, interval, period)
+        'Calculating %s for %s since %s @ interval:%s period:%d', indicator, ticker, since, interval, period)
     ticker = ticker.upper()
     query = {'ticker__iexact': ticker, 'interval__iexact': interval}
     if since:
@@ -50,84 +62,93 @@ def _calculate_sma(ticker: str, since: str | datetime = None, interval='1d', per
     _logger.debug('query:%s', query)
     tickers = TickerDailyInfo.objects(**query).order_by('trade_date')
     tickers_df = mongo_2_df(tickers)
-    # _logger.debug(tickers_df)
-    sma = tickers_df['close'].rolling(window=period).mean()
+
+    if indicator == 'sma':
+        values = tickers_df['close'].rolling(window=period).mean()
+    elif indicator == 'ema':
+        values = tickers_df['close'].ewm(span=period, adjust=False).mean()
+    else:
+        raise ValueError(f'Unsupported indicator: {indicator}')
+
     for i, t in enumerate(tickers):
-        # TODO: 优化这里的循环，使用正确的数据顺序+dropna 代替现在的完整遍历
-        if not numpy.isnan(sma[i]):
-            xp = f't.sma{period} = {sma[i]}'
-            exec(xp)
+        if not numpy.isnan(values[i]):
+            setattr(t, f'{indicator}{period}', values[i])
             t.save()
-        # else:
-        #     _logger.warning(f'Skip nan: {t.ticker} @{t.trade_date}')
 
 
-def _update_sma_with_details(ticker: str, interval: str = '1d', period: int = 20) -> bool:
+def _update_indicator_with_details(ticker: str, indicator: Literal['sma', 'ema'] = 'sma', interval: str = '1d',
+                                   period: int = 20) -> bool:
     """
-    Before calling this function, you should call update_tikers_daily_info
+    Before calling this function, you should call update_tikers_daily_info.
+    Updates the specified indicator for the given ticker.
     """
-    _logger.info('Updating sma for %s @ interval:%s period:%d',
-                 ticker, interval, period)
+    _logger.info('Updating %s for %s @ interval:%s period:%d',
+                 indicator, ticker, interval, period)
     try:
-        since = _get_since_trade_date_for_sma(
-            ticker, interval=interval, period=period)
-        _calculate_sma(ticker, since=since,
-                       interval=interval, period=period)
+        since = _get_since_trade_date_for_indicator(
+            ticker, indicator=indicator, interval=interval, period=period)
+        _calculate_indicator(
+            ticker, indicator=indicator, since=since,
+            interval=interval, period=period)
         return True
     except Exception as e:
-        _logger.error('Failed to update_sma for %s', ticker, exc_info=e)
+        _logger.error('Failed to update %s for %s', indicator, ticker, exc_info=e)
         return False
 
 
-def _update_sma_for_ticker(ticker: str) -> bool:
+def _update_ma_for_ticker(ticker: str) -> Dict[str, bool]:
     _logger.info('Updating sma for %s', ticker)
     make_db_connection()
     result = all(
         [
-            _update_sma_with_details(ticker, interval='1d', period=20),
-            _update_sma_with_details(ticker, interval='1d', period=50),
-            _update_sma_with_details(ticker, interval='1d', period=200),
+            _update_indicator_with_details(ticker, indicator='ema', interval='1d', period=10),
+            _update_indicator_with_details(ticker, indicator='ema', interval='1d', period=20),
+            _update_indicator_with_details(ticker, indicator='sma', interval='1d', period=10),
+            _update_indicator_with_details(ticker, indicator='sma', interval='1d', period=20),
+            _update_indicator_with_details(ticker, indicator='sma', interval='1d', period=50),
+            _update_indicator_with_details(ticker, indicator='sma', interval='1d', period=200),
         ]
     )
     _logger.info('Updated sma for %s:%s', ticker, result)
     return {ticker: result}
 
 
-def update_spx_daily_sma() -> bool:
+def update_daily_ma_by_idx(index_name: str) -> bool:
     try:
         make_db_connection()
         index_tickers: IndexTickers = IndexTickers.objects(
-            index_name='spx').order_by('-as_of_date').first()
+            index_name=index_name).order_by('-as_of_date').limit(1).first()
         if not index_tickers:
-            _logger.error('No index tickers found for spx')
+            _logger.error('No index tickers found for %s', index_name)
             return False
         to_update = index_tickers.tickers
         for i in range(3):
             # retry for max 3 times
-            _logger.info('Updating spx daily sma: %d -> %s', i, to_update)
+            _logger.info('Updating %s daily ma: %d -> %s', index_name, i, to_update)
             temp_results: list = None
             if is_in_daemon():
                 _logger.info('Using ThreadPoolExecutor')
                 with ThreadPoolExecutor() as executor:
                     temp_results = list(executor.map(
-                        _update_sma_for_ticker, to_update))
+                        _update_ma_for_ticker, to_update))
             else:
                 _logger.info('Using Process Pool')
                 with Pool(processes=os.cpu_count()) as p:
                     temp_results: list = p.map(
-                        _update_sma_for_ticker, to_update)
+                        _update_ma_for_ticker, to_update)
             results = {}
             for r in temp_results:
                 results.update(r)
             filtered_dict = {key: value for key,
-                             value in results.items() if not value}
+            value in results.items() if not value}
             to_update = list(filtered_dict.keys())
             if not to_update:
                 return True
             else:
-                _logger.info('Re-Updating spx daily sma: %s', to_update)
+                _logger.info('Re-Updating %s daily ma: %s', index_name, to_update)
+        return False
     except Exception as e:
-        _logger.error('Failed to update spx sma', exc_info=e)
+        _logger.error('Failed to update %s ma', index_name, exc_info=e)
         return False
 
 
@@ -136,4 +157,4 @@ class Indicators(SingletonParent):
         make_db_connection()
 
     def update_spx_daily_sma(self) -> bool:
-        return update_spx_daily_sma()
+        return update_daily_ma_by_idx('spx')
--- DataMiner/test/test_indicators.py
+++ DataMiner/test/test_indicators.py
@@ -1,4 +1,5 @@
 import unittest
+
 from dataminer import Indicators
 from detonator import make_db_connection, get_logger
 
@@ -7,13 +8,6 @@ _l = get_logger('IndicatorsTestCase')
 
 class IndicatorsTestCase(unittest.TestCase):
     make_db_connection()
-
-    def test_update_sma(self):
-        indicators: Indicators = Indicators.get_instance()
-        ret = indicators.update_sma('aapl')
-        ret = indicators.update_sma('aapl', period=50)
-        ret = indicators.update_sma('aapl', period=200)
-        _l.debug(ret)
 
     def test_update_spx_daily_sma(self):
         indicators: Indicators = Indicators.get_instance()
