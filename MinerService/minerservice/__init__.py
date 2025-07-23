d7883ec3da618d75bfda0588d8b6c3f7b533557b
Yuzheng Zhang <452613+zhangyuz@users.noreply.github.com>
2025-07-22 21:40:25 -0400

    Miner:fix: improve tasks timing
    ensure spx breadth updating timing
    
    Signed-off-by: Yuzheng Zhang <452613+zhangyuz@users.noreply.github.com>

--- MinerService/minerservice/__init__.py
+++ MinerService/minerservice/__init__.py
@@ -1,7 +1,7 @@
 from celery.schedules import crontab
 from minerworkers import app
 from dataminer.tasks import run_daily_updates_task, update_hk_all_task
-from celery import Celery, chain
+from celery import Celery
 from detonator import get_logger
 from marketbreadth.tasks import update_spx_market_breadth_task
 
@@ -17,30 +17,30 @@ app.autodiscover_tasks(['dataminer', 'marketbreadth'], force=True)
 
 
 @app.on_after_configure.connect
-def setup_periodic_tasks(sender: Celery, **kwargs):
+def setup_periodic_tasks(sender: Celery, **_):
     """
     Set up periodic tasks for Celery Beat.
     This function is connected to the `on_after_configure` signal.
     """
-    # Add the daily update task.
-    us_daily_chain = chain(
-        run_daily_updates_task.si(),
-        update_spx_market_breadth_task.si()
-    )
-
     _logger.info('Setting up miner service periodic tasks ...')
     sender.add_periodic_task(
-        crontab(hour=16, minute=30, day_of_week='mon-fri'),
-        us_daily_chain,
+        crontab(hour=16, minute=5, day_of_week='mon-fri'),
+        run_daily_updates_task.s(),
         name='us-daily-1630-updates',
         expires=600,
     )
 
-    # 17:15 Hong Kong time (Asia/Hong_Kong) explicitly set
     sender.add_periodic_task(
         crontab(hour=5, minute=1, day_of_week='mon-fri'),
         update_hk_all_task.s(),
         name='hk-daily-idx-updates',
+        expires=600,
+    )
+
+    sender.add_periodic_task(
+        crontab(hour="19-23", minute=30, day_of_week='mon-fri'),
+        update_spx_market_breadth_task.s(),
+        name='update-spx-market-breadth-hourly-evening',
         expires=600,
     )
     _logger.info('Miner service periodic tasks setup complete')
