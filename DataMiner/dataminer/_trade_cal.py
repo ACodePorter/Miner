0486d47ce4dd85e9172374e0a7089cea8bf65674
Yuzheng Zhang <452613+zhangyuz@users.noreply.github.com>
2025-08-01 20:48:40 -0400

    TradeCal:fix: use UTC for comparing clase time
    Signed-off-by: Yuzheng Zhang <452613+zhangyuz@users.noreply.github.com>

--- DataMiner/dataminer/_trade_cal.py
+++ DataMiner/dataminer/_trade_cal.py
@@ -182,5 +182,5 @@ class TradeCalendarShovel(SingletonParent):
     def get_last_closed_trade_date_before(self, day: str | datetime, country: str = 'us', exchange: str = 'XNYS') -> str:
         if isinstance(day, datetime):
             day = day.strftime('%Y%m%d')
-        now = datetime.now(self.EXCHANGE_TZ_MAP[exchange])
+        now = datetime.now(pytz.timezone('UTC'))
         return TradeCalendar.objects(cal_date__lte=day, is_open=True, country=country, exchange=exchange, close__lte=now).order_by('-cal_date').first().cal_date
